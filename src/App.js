8b05cde8a5e7125186fda4df7ccbb99245355d85
ColeNelson806 <colenelson806@outlook.com>
2023-07-21 09:45:58 -0500

    WIP ~ Working Buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,248 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex()
 
 function App() {
+  const [backState, setBackState] = useState(0);
+
+  const [listOfDex, setListOfDex] = useState(null); // array of all the dex options
+  const [showPokedexList, setShowPokedexList] = useState(false); // controls if dex list is displayed or not
+  const [selectedPokedex, setSelectedPokedex] = useState(null); // the dex that user selected/clicked on
+  const [selectedPokedexUrl, setSelectedPokedexUrl] = useState(null); // url of selected dex
+  
+  const [needPokemonListUpdate, setNeedPokemonListUpdate] = useState(null);
+  const [listOfPokemon, setListOfPokemon] = useState(null); // array of all the pokemons in the selected dex
+  const [showPokemonList, setShowPokemonList] = useState(false); // controls if pokemon list is displayed or not
+  const [selectedPokemon, setSelectedPokemon] = useState(null); // the pokemon the user selected/clicked on
+  
+  const [needPokemonDetailsListUpdate, setNeedPokemonDetailsListUpdate] = useState(null);
+  const [listOfPokemonDetails, setListOfPokemonDetails] = useState(null); // array of all the details of selected pokemon
+  const [showPokemonDetailsList, setShowPokemonDetailsList] = useState(false); // controls if pokemon details are displayed or not
+
+  
+
+
+  useEffect(() => {
+    FetchAPI().then((value) => {setListOfDex(value); updatePokedexList()});
+  }, []);
+
+
+
+  const updatePokedexList = () => {
+    setShowPokedexList(true);
+  };
+  const updateSelectedPokedex = (selected, selectedUrl) => {
+    setShowPokedexList(false);
+    setSelectedPokedex(selected);
+    setSelectedPokedexUrl(selectedUrl);
+    setNeedPokemonListUpdate(true);
+    setBackState(1);
+  };
+
+
+
+  const updatePokemonList = () => {
+    P.resource(selectedPokedexUrl).then((value) => {
+      setListOfPokemon(value.pokemon_entries);
+      setShowPokemonList(true);
+      setNeedPokemonListUpdate(false);
+    });
+  };
+  
+  const updateSelectedPokemon = (selected) => {
+    setShowPokemonList(false);
+    setSelectedPokemon(selected);
+    setNeedPokemonDetailsListUpdate(true);
+    setBackState(2);
+  };
+  if (needPokemonListUpdate === true) updatePokemonList();
+
+
+
+  const updatePokemonDetailsList = () => {
+      P.getPokemonByName(selectedPokemon).then((pokemonSpecific) => {
+        setListOfPokemonDetails(pokemonSpecific);
+        setShowPokemonDetailsList(true);
+        setNeedPokemonDetailsListUpdate(false);
+      }); 
+  };
+
+      
+  if (needPokemonDetailsListUpdate === true) updatePokemonDetailsList();
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h2 className="titlePokedex">{selectedPokedex ? `Selected Pokedex: ${selectedPokedex}` : "Select a Pokedex"}</h2>
+      <div className="outerDiv">
+        <div className="innerDiv">
+          <div className="displayDiv">
+            <div>
+              <button onClick={ () => {
+                HomeButton(setBackState, setShowPokedexList, setSelectedPokedex, setSelectedPokedexUrl, setNeedPokemonListUpdate, setListOfPokemon, setShowPokemonList, 
+                  setSelectedPokemon, setNeedPokemonDetailsListUpdate, setListOfPokemonDetails, setShowPokemonDetailsList)
+              }}>Home</button>
+
+              <button onClick={ () => {
+                BackButton(backState, setBackState, setShowPokedexList, setSelectedPokedex, setSelectedPokedexUrl, setNeedPokemonListUpdate, setListOfPokemon, setShowPokemonList, 
+                  setSelectedPokemon, setNeedPokemonDetailsListUpdate, setListOfPokemonDetails, setShowPokemonDetailsList)
+              }}>Back</button>
+            </div>
+            <br/>       
+            {showPokedexList ? PokedexList(listOfDex, updateSelectedPokedex) : null}
+            {showPokemonList ? PokemonList(listOfPokemon, updateSelectedPokemon) : null}
+            {showPokemonDetailsList ? PokemonDetails(listOfPokemonDetails) : null}
+
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function FetchAPI() {
+  const createDexList = async () => {
+    const dexPromise = await P.getPokedexsList();
+    return dexPromise.results;
+  }
+  return createDexList();
+};
+export { FetchAPI }; 
+
+function BackButton(backState, setBackState, setShowPokedexList, setSelectedPokedex, setSelectedPokedexUrl, setNeedPokemonListUpdate, setListOfPokemon, setShowPokemonList, 
+  setSelectedPokemon, setNeedPokemonDetailsListUpdate, setListOfPokemonDetails, setShowPokemonDetailsList) {
+  //backState === 1 ? setShowPokedexList(true) : backState === 2 ?  : null; 
+  if (backState === 0) {
+    alert("You can not go back any more.")
+  }
+
+  if (backState === 1) {
+    setBackState(0); setShowPokedexList(true); setSelectedPokedex(null); setSelectedPokedexUrl(null); setNeedPokemonListUpdate(null); 
+      setListOfPokemon(null); setShowPokemonList(false); 
+  }
+  if (backState === 2) {
+    setBackState(1); setShowPokemonList(true); setSelectedPokemon(null); setNeedPokemonDetailsListUpdate(null); 
+      setListOfPokemonDetails(null); setShowPokemonDetailsList(false);
+  }
+};
+
+function HomeButton(setBackState, setShowPokedexList, setSelectedPokedex, setSelectedPokedexUrl, setNeedPokemonListUpdate, setListOfPokemon, setShowPokemonList, 
+  setSelectedPokemon, setNeedPokemonDetailsListUpdate, setListOfPokemonDetails, setShowPokemonDetailsList) {
+    setBackState(0); setShowPokedexList(true); setSelectedPokedex(null); setSelectedPokedexUrl(null); setNeedPokemonListUpdate(null); setListOfPokemon(null); 
+      setShowPokemonList(false); setSelectedPokemon(null); setNeedPokemonDetailsListUpdate(null); setListOfPokemonDetails(null); setShowPokemonDetailsList(false);
+  }
+
+function PokedexList(listOfDex, updateSelectedPokedex) {
+  return (
+    listOfDex.map((currDex, index) => {
+    return <button key={ index } onClick={() => {
+      updateSelectedPokedex(currDex.name, currDex.url)
+    }}>{currDex.name}</button>
+    })
+  )
+};
+export { PokedexList };
+
+
+
+function PokemonList(listOfPokemon, updateSelectedPokemon) {
+  console.log(listOfPokemon);
+
+  return (
+    listOfPokemon.map((currPokemon) => {
+    return <button key={currPokemon.entry_number} onClick={() => {
+      updateSelectedPokemon(currPokemon.pokemon_species.name, currPokemon.pokemon_species.url)
+    }}>{currPokemon.pokemon_species.name}</button>
+    })
+  )
+};
+export { PokemonList };
+
+
+
+function PokemonDetails(listOfPokemonDetails) {
+  return (
+    <>
+      <PokemonSprite sprites={listOfPokemonDetails.sprites} selectedPokemon={listOfPokemonDetails.name}/>
+      <NameDetails nameObj={listOfPokemonDetails.name} pokemonId={listOfPokemonDetails.id}/>
+      <TypeDetails typesObj={listOfPokemonDetails.types}/>
+      <StatDetails statsObj={listOfPokemonDetails.stats}/>
+      <AbilityDetails abilitiesObj={listOfPokemonDetails.abilities}/>
+    </>
+  )
+};
+export { PokemonDetails };
+
+
+
+function PokemonSprite({sprites, selectedPokemon}) {
+  return <img src={sprites.front_default} alt={selectedPokemon.charAt(0).toUpperCase() + selectedPokemon.slice(1) + "'s sprite image"}/>
+}
+export { PokemonSprite }
+
+
+
+function NameDetails({nameObj, pokemonId}) {
+  return (
+    <div>
+      {
+        pokemonId.toString().length === 3 ? 
+        "#" + pokemonId : pokemonId.toString().length === 2 ? "#0" + pokemonId : "#00" + pokemonId
+      }
+
+      { " " + nameObj.charAt(0).toUpperCase() + nameObj.slice(1)}</div>
+  )
+}
+export { NameDetails }
+
+
+
+function TypeDetails({typesObj}) {
+  return (
+    <div>
+      Types:{typesObj.map((currTypeSlot, index) => { 
+        return (typesObj.length - index === 1) ?
+          " " + (currTypeSlot.type.name).charAt(0).toUpperCase() + currTypeSlot.type.name.slice(1) :
+          " " + (currTypeSlot.type.name).charAt(0).toUpperCase() + currTypeSlot.type.name.slice(1) + "  -- ";
+       })}
+    </div>
+    
+  )
+}
+export { TypeDetails }
+
+
+
+function StatDetails({statsObj}) {
+  return (
+    statsObj.map((currStat, index) => {
+      return (
+        <div key={ index }>
+          {(currStat.stat.name.charAt(0).toUpperCase() + currStat.stat.name.slice(1)) + ": " + currStat.base_stat}
+        </div>
+      )
+    })
+  )
+}
+export { StatDetails }
+
+
+
+function AbilityDetails({abilitiesObj}) {
+  return (
+    <div>
+      Abilities:{abilitiesObj.map((currAbilitySlot, index) => {
+        return (abilitiesObj.length - index === 1) ?  
+        " " + currAbilitySlot.ability.name.charAt(0).toUpperCase() + currAbilitySlot.ability.name.slice(1) :
+        " " + currAbilitySlot.ability.name.charAt(0).toUpperCase() + currAbilitySlot.ability.name.slice(1) + "  -- ";
+      })}
+    </div>
+  )
+}
+export { AbilityDetails }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,102 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const pokemonsObj = [ 
+  //   { entry_number: 1, pokemon_species: { name: "pikipek", url: "https://pokeapi.co/api/v2/pokemon-species/731/" } },
+  //   { entry_number: 2, pokemon_species: { name: "inkay", url: "https://pokeapi.co/api/v2/pokemon-species/686/" } },
+  //   { entry_number: 3, pokemon_species: { name: "toucannon", url: "https://pokeapi.co/api/v2/pokemon-species/733/" } },
+  //   { entry_number: 4, pokemon_species: { name: "yungoos", url: "https://pokeapi.co/api/v2/pokemon-species/734/" } },
+  //   { entry_number: 5, pokemon_species: { name: "gumshoos", url: "https://pokeapi.co/api/v2/pokemon-species/735/" } },
+  //   { entry_number: 6, pokemon_species: { name: "rattata", url: "https://pokeapi.co/api/v2/pokemon-species/19/" } },
+  //   { entry_number: 7, pokemon_species: { name: "raticate", url: "https://pokeapi.co/api/v2/pokemon-species/20/" } },
+  //   { entry_number: 8, pokemon_species: { name: "buneary", url: "https://pokeapi.co/api/v2/pokemon-species/427/" } },
+  //   { entry_number: 9, pokemon_species: { name: "lopunny", url: "https://pokeapi.co/api/v2/pokemon-species/428/" } },
+  //   { entry_number: 10, pokemon_species: { name: "inkay", url: "https://pokeapi.co/api/v2/pokemon-species/686/" } }
+  // ]
+
+describe("App", () => {
+  describe("Requirement 1", () => {
+    test("Render list of Pokedexes when application is loaded the first time", () => {
+      // When the app is rendered intially
+      //render(<App />)
+      
+      // Then
+      //Renders list of pokedexes returned from the API
+    });
+  });
+
+  describe("Requirement 2", () => {
+    describe("API throws error when rendering list of Pokedexs", () => {
+      test("User is shown an error message", () => {
+      // Given user attempting to select a Pokedex
+      // When an error occurs from the API
+      // Then they are presented an error message
+      // And the list of Pokedexes does not render
+      });
+
+      test("List of pokedexes does not render", () => {
+
+      });
+    });
+  });
+
+  describe("Requirement 3", () => {
+    test("When a View button is clicked the associated Pokedex is stored", () => {
+      
+    });
+  });
+
+  describe("Requirement 4", () => {
+    test("When a View button is clicked, the associated Pokedex's Pokemon is rendered", () => {
+      
+    });
+  });
+
+  describe("Requirement 5", () => {
+    describe("API throws error when selecting Pokemon", () => {
+      test("User is shown error message", () => {
+        
+      });
+
+      test("Pokemon list does not render", () => {
+
+      });
+    });
+  });
+
+  describe("Requirement 6", () => {
+    test("When a View Details button is clicked the associated Pokemon is stored", () => {
+      //
+    });
+  });
+
+  describe("Requirement 7", () => {
+    test("When a View Details button is clicked the associated Pokemon's details is rendered", () => {
+      //
+    });
+  });
+
+  describe("Requirement 8", () => {
+    describe("API throws error when rendering selected Pokemon's details", () => {
+      test("User is shown error message", () => {
+        
+      });
+
+      test("Pokemon's details does not render", () => {
+        
+      });
+    });
+  });
+
+  describe("Requirement 9", () => {
+    test("User is returned to first screen when Home button is clicked", () => {
+      //
+    });
+  });
+
+  describe("Requirement 10", () => {
+    test("User is moved back one screen when Back button is clicked", () => {
+      //
+    });
+  });
 });
